7295b0e5aecf13ad014833b636c7d1204e51220f
YJ <yj2023@kentech.ac.kr>
2024-11-24 17:46:00 +0900

    final push

--- assigned/test_run.sh
+++ assigned/test_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-models=("resnet50")
+models=("resnet" "resnet18")
 batch_sizes=(64 32)
 learning_rates=(0.001 0.01 0.1)
 epochs=(150 100 50)
@@ -13,7 +13,7 @@ do
     do
       for epoch in "${epochs[@]}"
       do
-        python cifar_test_test.py --model $model --batch_size $batch_size --learning_rate $lr --num_epoch $epoch
+        python3 cifar_training.py --model $model --batch_size $batch_size --learning_rate $lr --num_epoch $epoch
       done
     done
   done
